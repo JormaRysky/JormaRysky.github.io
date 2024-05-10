7672cdd5afa88d375fa2e499b69fe5d4331172ad
JormaRysky <145664092+JormaRysky@users.noreply.github.com>
2024-05-10 16:28:23 +0300

    asd

--- EK2/slider.js
+++ EK2/slider.js
@@ -11,7 +11,7 @@ burger.addEventListener("click", () => {
 });*/
 
 //Environments
-
+/*
 let currentenvIndex = 0;
 
 const envimages = document.querySelectorAll('.envkuva');
@@ -42,18 +42,47 @@ function nextenvImage() {
   showenvImage(currentenvIndex);
 }
 
-showenvImage(currentenvIndex);
+showenvImage(currentenvIndex);*/
 
 //galleria
 
 let currentgalIndex = 0;
-
+ 
 const galimages = document.querySelectorAll('.galkuva');
 const totalgalImages = galimages.length;
 
 
 const galname = document.getElementById('currentenvtitle');
-const galnames = ["Tar Pits Over The Void", "Finnish Hoaxes", "End Of The World Carnevals","Ghost Canyon"];
+const galnames = [
+"Graveyard",
+"Daytime Test",
+"Collisions Demo",
+"Hurt Effects",
+"Awesome Level Design",
+"Sea",
+"Attack Animation",
+"Hot Rods Compared",
+"Fog Tech",
+"Reflections",
+"Level Format from EK - LH",
+"Making of Sprites",
+"Totally awesome sketches"
+,];
+const galdescriptions = [
+"Post processing test using EK-LH assets                                                                                                                                                  ",
+"Test render of fog combined with bright environment                                                                                                                                      ",
+"Demonstrations of use of box colliders for achieving true slopes with primitive level format                                                                                             ",
+"Demonstration of possible hit effect employing momentary chromatic aberration                                                                                                            ",
+"Demonstration of speed run idea the game is built on                                                                                                                                     ",
+"Very primitive water simulation                                                                                                                                                          ",
+"Player's frame by frame-animated UI-hand                                                                                                                                                 ",
+"EK2 car compared against one from EK-LH, which is -just- little bit lower fidelity                                                                                                       ",
+"Demonstration of very primitive fog simulation, employing meshes and semialpha textures                                                                                                  ",
+"Demonstration of reflections that are actually just duplicated and mirrored meshes                                                                                                       ",
+"Level editor of EK-LH during jam was Google Docs. Docs allowed some amount of visualization cause it was possible to give color for numbers, but otherwise it was all visualized in head.",
+"Rotoscoping was used for most sprite graphics                                                                                                                                            ",
+"made for post jam EK-LH                                                                                                                                                                  "
+ ];
 
 function showgalImage(index) {
     galimages.forEach((image, i) => {
@@ -64,6 +93,7 @@ function showgalImage(index) {
       }
     })
     document.getElementById("currentgaltitle").innerHTML = "<h3>" + galnames[currentgalIndex] + "</h3>";
+    document.getElementById("currentgaldescr").innerHTML = "<h3>" + galdescriptions[currentgalIndex] + "</h3>";
   }
   
   function prevgalImage() {
@@ -77,3 +107,5 @@ function showgalImage(index) {
   }
   
   showgalImage(currentgalIndex);
+
+  
